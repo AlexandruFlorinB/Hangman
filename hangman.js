407362603af4b2c83b1563cb133edd819707ffc6
AlexandruFlorinB <101994570+AlexandruFlorinB@users.noreply.github.com>
2022-03-31 20:05:59 +0300

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -1,67 +1,69 @@
-let wordList = ["american", "bolovan", "armament", "european", "muzician", "filozof", "bombardament", "trandafir", "fotbal", "inginerie", "armonie", "cronologie", "matematica", "bucurie", "pilot", "informatica"]
-let getWord = wordList[Math.floor(Math.random() * wordList.length)]
-
+let wordsList = ["american", "bolovan", "armament", "european", "muzician", "filozof", "bombardament", "trandafir", "fotbal", "inginerie", "armonie", "cronologie", "matematica", "bucurie", "pilot", "informatica"]
+let wordToGuess = wordsList[Math.floor(Math.random() * wordsList.length)]
 let lines
-let nrLifes = 5
-let textShown = []
-let wrongLetter = []
+let nrLives = 5
+let textDisplayed = []
+let wrongLetters = []
 
+function printInterface() {
+    wordToGuess = wordsList[Math.floor(Math.random() * wordsList.length)]
+    alert(wordToGuess)
+    underLines(wordToGuess)
+}
 
-function generateWord() {
-    getWord = wordList[Math.floor(Math.random() * wordList.length)]
-    for (let i = 0; i < getWord.length; ++i) {
-        textShown[i] = "_"
+function underLines(wordToGuess) {
+    for (let i = 0; i < wordToGuess.length; ++i) {
+        textDisplayed[i] = "_"
     }
-    lines = textShown.join(" ")
+    lines = textDisplayed.join(" ")
     document.getElementById("generate").innerHTML = lines
 }
 
 function letter() {
-    var letter = document.getElementById("letter").value
-
+    let letter = document.getElementById("letter").value
     if (letter.length > 0) {
         let flag = 0
-        for (let i = 0; i < getWord.length; ++i) {
-            if (getWord[i] === letter) {
-                textShown[i] = letter
+        for (let i = 0; i < wordToGuess.length; ++i) {
+            if (wordToGuess[i] === letter) {
+                textDisplayed[i] = letter
                 flag = 1
             }   
         }
         if (flag === 0) {
-            wrongLetter.push(letter)
-            document.getElementById("recicled").value = wrongLetter
+            wrongLetters.push(letter)
+            document.getElementById("recicled").value = wrongLetters
             flag = 1
-            --nrLifes
+            --nrLives
         }
-        document.getElementById("generate").innerHTML = textShown.join(" ")
-        document.getElementById("lifes").value = nrLifes
+        document.getElementById("generate").innerHTML = textDisplayed.join(" ")
+        document.getElementById("lives").value = nrLives
         document.getElementById("letter").value = "" 
     }
-    if (nrLifes === 0) {
-        document.getElementById("lives").innerHTML = "You died"
+    if (nrLives === 0) {
+        document.getElementById("message").innerHTML = "You died"
     } 
     flag = 0
-    for (let i = 0; i < getWord.length; ++i) {
-        if (textShown[i] === "_") {
+    for (let i = 0; i < wordToGuess.length; ++i) {
+        if (textDisplayed[i] === "_") {
             flag = 1
             break
         }
     }
     if (flag === 0) {
-        document.getElementById("lives").innerHTML = "You won"
+        document.getElementById("message").innerHTML = "You won"
     }   
 }
 
 function resetPage() {
-    textShown = []
-    wrongLetter = []
-    nrLifes = 5
+    textDisplayed = []
+    wrongLetters = []
+    nrLives = 5
     document.getElementById("generate").value = ""
     document.getElementById("letter").value = ""
     document.getElementById("recicled").value = ""
-    document.getElementById("lifes").value = 5
-    document.getElementById("lives").innerText = ""
-    generateWord()
+    document.getElementById("lives").value = 5
+    document.getElementById("message").innerText = ""
+    printInterface()
 }
 
 
